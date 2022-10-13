a06c7a022e3f012a13467304759176fbf0bf8ae3
Alex Waters <contact@alexwaters.dev>
2022-10-13 01:06:19 -0500

    added support to send a meme one a day

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
+from time import sleep
 from local_secrets import local_secrets
+
+import requests
+import re
+sub_reddit="memes"
+
+user_agent = {'User-agent': 'Mozilla/5.0'}
+x = requests.get('https://old.reddit.com/r/'+sub_reddit+'/top/?sort=top&t=day', headers = user_agent)
+
+code=x.text[x.text.find("thing id-t3")+12:x.text.find("thing id-t3")+18]
+
+sleep(1)
+x = requests.get('https://old.reddit.com/r/'+sub_reddit+'/comments/'+code+'/'+sub_reddit+'/', headers = user_agent)
+link = x.text[x.text.find("post-link")-54:x.text.find("post-link")-19]
+
+
+'''
 import openai
 
 openai.api_key = local_secrets['openai']
@@ -16,3 +33,4 @@ completion = openai.Completion.create(engine="text-ada-001",
     )
 
 print(completion.choices[0].text)
+'''
